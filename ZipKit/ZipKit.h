c7e2827cdd5284f05cd2f23c3fad484ff965e926
Sam Deane <sam@elegantchaos.com>
2013-10-29 14:00:33 +0000

    Wrap AppleDouble stuff with #if ZK_TARGET_OS_MAC

--- ZipKit/ZipKit.h
+++ ZipKit/ZipKit.h
@@ -5,18 +5,20 @@
 //  Created by Sam Deane on 25/10/13.
 //
 
-#import "ZKDefs.h"
 #import "ZKArchive.h"
 #import "ZKDataArchive.h"
+#import "ZKDefs.h"
 #import "ZKFileArchive.h"
 #import "ZKLog.h"
 
-#import "GMAppleDouble.h"
-
-#import "GMAppleDouble+ZKAdditions.h"
 #import "NSData+ZKAdditions.h"
 #import "NSDate+ZKAdditions.h"
 #import "NSDictionary+ZKAdditions.h"
 #import "NSFileHandle+ZKAdditions.h"
 #import "NSFileManager+ZKAdditions.h"
 #import "NSString+ZKAdditions.h"
+
+#if ZK_TARGET_OS_MAC
+#import "GMAppleDouble.h"
+#import "GMAppleDouble+ZKAdditions.h"
+#endif
