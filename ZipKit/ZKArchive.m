5a83d53a6cf561f99c65cd93d32617c160a9b150
Nick Brook <nrbrook@gmail.com>
2017-02-06 16:27:04 +0000

    Removed use of deprecated API

--- ZipKit/ZKArchive.m
+++ ZipKit/ZKArchive.m
@@ -24,7 +24,7 @@
 	NSData *fileHeader = [fileHandle readDataOfLength:4];
 	[fileHandle closeFile];
 	UInt32 headerValue;
-	[fileHeader getBytes:&headerValue];
+	[fileHeader getBytes:&headerValue length:sizeof(headerValue)];
 	return CFSwapInt32LittleToHost(headerValue) == ZKLFHeaderMagicNumber;
 }
 
